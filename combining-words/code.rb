183ae32c40bcc9053df637fe8fadc8ab0099ffd9
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-07-09 13:53:59 -0400

    more

---
+++ combining-words/code.rb
@@ -0,0 +1,2 @@
+#Your code here
+puts "I " +  "really " + "hate " + "string " +  'concatonation '+ "because "+ "its "+ "a "+ "PITA"
---
+++ planet-landing-summary/code.rb
@@ -0,0 +1,11 @@
+# YOUR CODE GOES HERE
+date_left = "july 21"
+date_arrived = "july 21"
+people_met = "cute queers"
+girlfriends_mood = "pissed"
+result = "married"
+place_traveledto="montreal"
+place_proposed = "waterfall"
+
+
+puts "on #{date_left} I went to #{place_traveledto} and the whole idea was to explore the sites and maybe some #{people_met} and decide if myself and the girlfriend should move there. well  since i went on tinder the girlfriend was #{girlfriends_mood} but she didin't know I'd take her to a #{place_proposed} to ask if we'd get #{result}"
