4499eea752e942d497e92299971efb8a6185a909
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-07-16 20:59:08 -0400

    more

--- smart-vending-machine/code.rb
+++ smart-vending-machine/code.rb
@@ -1,1 +1,33 @@
 # YOUR CODE GOES HERE
+#
+# puts "what items do you want dude from this vending machine"
+# request = gets.chomp
+# puts "ok n how much do you want of that. how many"
+# number = gets.chomp.to_i
+#
+#
+# number.times do
+#     puts "#{request}"
+# end
+# puts "ok  come again dude"
+
+
+
+puts "what items do you want dude from this vending machine"
+request = gets.chomp
+puts "ok n how much do you want of that. how many"
+number = gets.chomp.to_i
+
+if number == "tons"
+    number == rand(20)
+    number.times do
+        puts "#{request}"
+else
+number.times do
+    puts "#{request}"
+end
+
+puts "ok  come again dude"
+end
+end
+end
