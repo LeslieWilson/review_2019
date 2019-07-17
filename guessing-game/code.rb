8eb239997a038fe465242dfe7a1f13c3746ba95a
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-07-16 21:19:19 -0400

    more

---
+++ guessing-game/code.rb
@@ -0,0 +1,29 @@
+# Your code goes here
+
+require 'pry'
+
+
+score = 0
+puts "what level difficulty do you want to play, easy or hard"
+level = gets.chomp
+if level == 'easy'
+    number = rand(10) +1
+    statement = "between 1 and 10"
+elsif level == 'hard'
+        number = rand(20) +1
+        statement = 'between 1 and 20'
+
+end
+puts "I picked a random number #{statement} "
+puts "please enter the number you think I picked"
+
+guess = gets.chomp.to_i
+
+while guess != number
+    puts "you guessed wrong, enter another number"
+    score +=  1
+    guess = gets.chomp.to_i
+    binding.pry
+end
+puts "awesome you guessed rigt!"
+puts "your score is #{score}"
