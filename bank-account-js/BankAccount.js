c02f4b30125df8942c8929a90ebe1ee7059cf344
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-08-24 13:28:56 -0400

    more

--- bank-account-js/BankAccount.js
+++ bank-account-js/BankAccount.js
@@ -1,29 +1,29 @@
 class BankAccount{
     constructor(initialBalance){
-    this.transactions = transactions
-    transactionList = []
-    transactionList.push(transactions)
-
-
-
-
-    addATransaction{
-        constructor(number){
-            this.number = number
-        transactionList.push(number)
-
+        this.initialBalance  = initialBalance
+        this.transactionList = []
     }
 
+    addATransaction(number){
+        this.transactionList.push(number)
+    }
 
     currentBalance(){
-
+        this.transactionList.forEach(function(transaction){
+        this.initialBalance += transaction
+    })
+        return (this.initialBalance)
     }
-
-    let initialBalance = 1000
-    let bills = [-45, -99.95, -34.43]
-
-
 }
 
 
-console.log (BankAccount.addATransaction(75))
+
+
+
+
+
+let initialBalance = 1000
+let bills = [-45, -99.95, -34.43]
+
+newAccount = new BankAccount(initialBalance)
+console.log(newAccount.currentBalance())
