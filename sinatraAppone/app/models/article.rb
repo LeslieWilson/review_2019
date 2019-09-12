e95ecd08b2ab550ba468c8c6c4f37a7959026979
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-09-11 21:55:05 -0400

    sinatratry

--- sinatraAppone/app/models/article.rb
+++ sinatraAppone/app/models/article.rb
@@ -1,2 +1,9 @@
 class Article
+    attr_accessor :description, :title, :url
+    def initialize(description, title,url)
+        @description = description
+        @title = title
+        @url=url
+
+end
 end
--- sinatraAppone/server.rb
+++ sinatraAppone/server.rb
@@ -21,28 +21,51 @@ end
 #         erb :index
 #     end
 
-get '/articles' do
-    @articles =[]
-    CSV.foreach("articles.csv", headers: true) do |row|
-        new_article = Article.new(description, title,url)
-        @articles << new_article
-    erb :index
-end
+# get '/articles' do
+#     @articles =[]
+#     CSV.foreach("articles.csv", headers: true) do |row|
+#         new_article = Article.new(
+#             row["description"],
+#             row["title"],
+#             row["URL"]
+#             )
+#         @articles << new_article
+#     end
+#     erb :index
+# end
 
 
 # version 1- doesn't give you versitle restaurant objects
 
-# get '/articles' do
-#
-#     @articles = CSV.readlines("articles.csv", headers: true)
-#
-#   erb :index
-# end
-#
-# get '/new' do
-#     erb :new
-# end
-#
+get '/articles' do
+
+    @articles = CSV.readlines("articles.csv")
+
+  erb :index
+end
+
+get '/articles/new' do
+    erb :new
+end
+
+post "/articles/new" do
+    @title = params["article_title"]
+    @url = params["article_url"]
+    @description = params["article_description"]
+
+if @title == "" || @url==""||@description == ""
+    @error = "please fill out forms correctly"
+    erb :new
+else
+
+    CSV.open("articles.csv", "a") do |csv|
+        csv << [title,url,description]
+    end
+    redirect "/articles"
+end
+
+end
+
 # post '/articles' do
 #   article = params['article']
 #   File.open('articles.txt', 'a') do |file|
