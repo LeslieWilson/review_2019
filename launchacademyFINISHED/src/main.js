9577fcbe5ae4c534d9673ea2431231b255176e42
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-09-27 13:58:30 -0400

    react router

--- launchacademyFINISHED/src/main.js
+++
@@ -1,9 +0,0 @@
-import './main.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
